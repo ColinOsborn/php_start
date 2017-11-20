c5e1a40443ae16f93ab94f0647b99760d2a8ffc4
Colin Osborn <colin.osborn@gmail.com>
2017-11-19 17:02:16 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Learning php</title>
+  </head>
+  <body>
+    <p><?php $name = "Ajay"; ?></p>
+    <!-- Varibles all start in php with $ sign -->
+    <p><?php echo name; ?></p>
+  </body>
+</html>
