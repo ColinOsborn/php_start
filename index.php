b871c0d525339b5cc8e9cac20ed89b53490534d7
Colin Osborn <colin.osborn@gmail.com>
2017-11-30 00:03:51 -0700

    concatanation

--- index.php
+++ index.php
@@ -5,8 +5,12 @@
     <title>Learning php</title>
   </head>
   <body>
-    <p><?php $name = "Ajay"; ?></p>
+    <p><?php $name = 'Ajay'; ?></p>
     <!-- Varibles all start in php with $ sign -->
-    <p><?php echo name; ?></p>
+    <p><?php echo $name; ?></p>
+    <!-- <li><?php echo 'Favorite pet name: ' . $name; ?></li> -->
+    <!-- this will work as string concatanation, however, there is an easier way, below -->
+    <li><?php echo "Favorite Pet Name: $name "; ?></li>
+    <!-- Variables will print if you wrap them in double quotes, like ruby -->
   </body>
 </html>
